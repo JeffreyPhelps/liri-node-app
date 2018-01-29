8f584c3754acf34143c782beae6e163d36c0e0d9
Jeffrey Phelps <phelpsjeffrey@yahoo.com>
2018-01-29 08:50:01 -0700

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,6 +1,11 @@
 exports.twitterKeys = {
-    consumer_key: 'mbg1foZtVoYsrIZ8MI6oASa9n',
-    consumer_secret: 'DUh47moCTCw3lx6ye3Gio9o2oOt8izsJUmdSPcQDj7sbkcdRU3',
-    access_token_key: '757763396972445696-IXzAYCtbkd9MiF7h9xEZjxdRxGq6i1P',
-    access_token_secret: 'GEaGG6XP0vXwd6pD3MAYERClJsRWsheEV4LRzhMRaRluA',
+    consumer_key: 'RuC3iJiXqyPRttQgnzrBUo01S',
+    consumer_secret: 'YLY7JcVEJ4fT391CSqpIWJBvaa6O7uluPSAulIZUgRZJSVFFcc',
+    access_token_key: '74389406-eOz68dLx1UCcGhR8SxZNVlCk6kYuVtAaAcaLVvB6s',
+    access_token_secret: '4AiDJmX5secjGFjtIR7ZryQwkyTYCHmnAO9TJP1uEDQzg',
   }
+
+
+// Spotify API Keys
+//  Client ID bf94c178a64a4055aeddbbd772e7c4c9
+//  Client Secret 02cbe6ff4aff4b8fa854f06310fb02f9
